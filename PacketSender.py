09dd51a4eb7c60b5894edc423db46dad75ca22be
delphienator <delphienator@gmail.com>
2020-12-06 21:13:28 +0000

    version 0.2 - finalized function get MAC, IP

--- PacketSender.py
+++ PacketSender.py
@@ -1,5 +1,6 @@
 import binascii
 import socket
+import netifaces as ni
 
 # defining functions
 
@@ -7,8 +8,12 @@ def getInterfaceInfo(iface_name):
     """This function will read actual NIC configuration as source MAC and source IP.
     Input parameters: interface name, from which we send packets.
     Return: current MAC, current IP """
+    ip = ni.ifaddresses(iface)[ni.AF_INET][0]['addr']
+    netmask = ni.ifaddresses(iface)[ni.AF_INET][0]['netmask']
+    iface_mac=ni.ifaddresses(iface)[ni.AF_LINK][0]['addr']
+#    print(ip, netmask, iface_mac) # debug only
+    return(iface_mac, ip)
 
-    pass
 
 def send_packet(dest_ip,dest_port,payload):
     """" This function will generate packet from a specified interface, basing on next input parameters:
@@ -22,6 +27,7 @@ def send_packet(dest_ip,dest_port,payload):
 # source interface, destination IP, dest port, payload
 
 iface=str(input("Enter your source interface:"))
-dest_ip=str(input("Enter your destination interface:"))
-dest_port=str(input("Enter your destination port:"))
-payload=str(input("Enter UDP payload of your packet in HEX format:"))
+#dest_ip=str(input("Enter your destination interface:"))
+#dest_port=str(input("Enter your destination port:"))
+#payload=str(input("Enter UDP payload of your packet in HEX format:"))
+
