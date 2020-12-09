1ddb7197be6ef9776d15daeffef0d566352c750b
delphienator <delphienator@gmail.com>
2020-12-09 22:06:53 +0000

    Version 0.3 - sending a simple UDP packet with dummy payload

--- PacketSender.py
+++ PacketSender.py
@@ -16,18 +16,32 @@ def getInterfaceInfo(iface_name):
 
 
 def send_packet(dest_ip,dest_port,payload):
-    """" This function will generate packet from a specified interface, basing on next input parameters:
+    """ This function will generate packet from a specified interface, basing on next input parameters:
         Input: dest_ip,dest_port,payload
         Output: status of sending if successful
     """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)    # creating a socket
+
+                      					       # Let's send data through UDP protocol
+   # while True:
+    s.sendto(payload.encode('utf-8'), (dest_ip, dest_port))
+    print("\n\n Packet sent : ", payload, "\n\n")
+    s.close()
+
     pass
 
 
 # reading the input parameters:
 # source interface, destination IP, dest port, payload
 
-iface=str(input("Enter your source interface:"))
+#iface=str(input("Enter your source interface:"))
 #dest_ip=str(input("Enter your destination interface:"))
-#dest_port=str(input("Enter your destination port:"))
+#dest_port=int(input("Enter your destination port:"))
 #payload=str(input("Enter UDP payload of your packet in HEX format:"))
 
+dest_ip="172.1.1.1"
+dest_port=4000
+payload="1234567890"
+
+
+send_packet(dest_ip,dest_port,payload)
